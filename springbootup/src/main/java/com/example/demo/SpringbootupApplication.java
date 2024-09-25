6d07c374eeae251b0ce9fd4a3e82b2f166280975
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-09-25 15:23:08 +0800

    feat: AOP @Before and @Pointcut

--- springbootup/src/main/java/com/example/demo/SpringbootupApplication.java
+++ springbootup/src/main/java/com/example/demo/SpringbootupApplication.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.example.demo.dao.AccountDAO;
+import com.example.demo.dao.MembershipDAO;
 
 @SpringBootApplication
 public class SpringbootupApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootupApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootupApplication.class, args);
+  }
 
+  @Bean
+  public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+    return runner -> {
+      demoTheBeforeAdvice(accountDAO, membershipDAO);
+    };
+  }
+
+  private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+    // call the business method
+    Account myAccount = new Account();
+    theAccountDAO.addAccount(myAccount, true);
+    theAccountDAO.doWork();
+
+    // call the accountdao getter/setter methods
+    theAccountDAO.setName("foobar");
+    theAccountDAO.setServiceCode("silver");
+
+    String name = theAccountDAO.getName();
+    String code = theAccountDAO.getServiceCode();
+
+    // call the membership business method
+    theMembershipDAO.addSillyMember();
+    theMembershipDAO.goToSleep();
+
+  }
 }
---
+++ springbootup/src/main/java/com/example/demo/dao/MembershipDAO.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao;
+
+public interface MembershipDAO {
+  boolean addSillyMember();
+
+  void goToSleep();
+}
