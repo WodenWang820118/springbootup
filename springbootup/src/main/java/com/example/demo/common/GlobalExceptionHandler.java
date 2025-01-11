6b29e0626b15961ee0755f5684050379cb44b5ad
Guan-Xin Wang <guanxinwang0118@gmail.com>
2025-01-11 14:43:15 +0800

    refactor: nx update; reorganize classes

--- springbootup/src/main/java/com/example/demo/common/GlobalExceptionHandler.java
+++ springbootup/src/main/java/com/example/demo/common/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package com.example.demo.common;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
